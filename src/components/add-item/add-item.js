f32ba6c4b88f1ecfd8b1127a227f0224f7b3b0c2
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-08-06 12:57:27 +0300

    feat: implement function to adding new item in todo list

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -4,14 +4,31 @@ import './add-item.css';
 
 export default class AddItem extends Component {
 
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    };
+
     render() {
         return (
-            <div className="add-item" >
-                <button className="btn btn-outline-secondary"
-                        onClick={() => this.props.onItemAdded('Hello')} >
+            <form className="add-item d-flex" onSubmit={this.onSubmit} >
+                <input type="text"
+                       className="form-control"
+                       onChange={this.onLabelChange} placeholder="What needs to be done" />
+                <button className="btn btn-outline-secondary">
                     Add Item
                 </button>
-            </div>
+            </form>
         );
     }
 };
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,16 +6,26 @@ import ItemStatusFilter from '../item-status-filter';
 import './app.css';
 import AddItem from "../add-item";
 
+
 export default class App extends Component {
 
     maxId = 100;
 
     state = {
         todoData: [
-            { label: 'Drink Coffee', important: false, id: 1 },
-            { label: 'Make Awesome App', important: true, id: 2 },
-            { label: 'Have a lunch', important: false, id: 3 }
+            this.createTodoItem('Drink Coffee'),
+            this.createTodoItem('Make Awesome App'),
+            this.createTodoItem('Have a lunch')
         ]
+    }
+
+    createTodoItem(label) {
+        return {
+            label,
+            important: false,
+            done: false,
+            id: this.maxId++
+        }
     }
 
     deleteItem = (id) => {
@@ -33,11 +43,7 @@ export default class App extends Component {
     }
 
     addItem = (text) => {
-        const newItem = {
-            label: text,
-            important: false,
-            id: this.maxId++
-        };
+        const newItem = this.createTodoItem(text);
 
         this.setState(({todoData})=>{
             const newArr = [
@@ -51,24 +57,50 @@ export default class App extends Component {
         });
     }
 
+    toggleProperty(arr, id, propName) {
+        const idx = arr.findIndex((el) => el.id === id);
+
+        const oldItem = arr[idx];
+        const newItem = {...oldItem, [propName]: !oldItem[propName]};
+
+        return [
+            ...arr.slice(0, idx),
+            newItem,
+            ...arr.slice(idx + 1)
+        ];
+    }
+
     onToggleImportant = (id) => {
-        console.log('imp', id)
+        this.setState(({todoData})=>{
+            return {
+                todoData: this.toggleProperty(todoData, id, 'important')
+            };
+        });
     }
 
     onToggleDone = (id) => {
-        console.log('done', id)
-    }
+        this.setState(({todoData})=>{
+            return {
+                todoData: this.toggleProperty(todoData, id, 'done')
+            };
+        });
+    };
 
     render() {
+
+        const { todoData } = this.state;
+        const doneCount = todoData.filter((el) => el.done).length;
+        const todoCount = todoData.length - doneCount;
+
         return (
             <div className="todo-app">
-                <AppHeader toDo={1} done={3} />
+                <AppHeader toDo={todoCount} done={doneCount} />
                 <div className="top-panel d-flex">
                     <SearchPanel />
                     <ItemStatusFilter />
                 </div>
 
-                <TodoList todos={this.state.todoData}
+                <TodoList todos={todoData}
                           onDeleted={this.deleteItem}
                           onToggleImportant={this.onToggleImportant}
                           onToggleDone={this.onToggleDone} />
